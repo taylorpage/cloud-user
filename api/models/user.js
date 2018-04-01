8f45e2ee1fbcecf720c82a2719ea0fe27ab02e35
Taylor Page <jonathantaylorpage@gmail.com>
2018-03-31 22:24:47 -0500

    (feat): Create user model
    Create a user model using mongoose

---
+++ api/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// User schema
+const UserSchema = new Schema({
+    email: String,
+    password: String,
+    affiliate: String
+});
+
+// User model
+const UserModel = mongoose.model('User', UserSchema);
+
+// User constructor
+const User = (userData) => {
+    return UserModel.create(userData, (err, instance) => {
+        return err ? err : instance;
+    });
+}
+
+module.exports = {
+    User
+}
